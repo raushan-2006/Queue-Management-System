e0f804633d6a1e2445f250cc41ce570713e9a525
raushan kumar <raushankumar@raushans-MacBook-Air.local>
2026-03-26 16:45:22 +0530

    queue project

--- server/config/database.js
+++ server/config/database.js
@@ -1,16 +1,14 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 require("dotenv").config();
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
 
-const connectDb= async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDb Connected");
-
-    }
-    catch(error){
-        console.log(error);
-        process.exit(1);
-
-    }
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
 };
-module.exports=connectDb;
+
+module.exports = connectDB;
